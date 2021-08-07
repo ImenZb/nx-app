76f78027159cde5039b563d3a3101d3dc3db3706
ImenZb <imen.zbidi.b@gmail.com>
2021-08-07 21:49:10 +0200

    init

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListPageComponent } from './pages/product-list-page/product-list-page.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+const routes: Routes = [
+  {
+    path: '', component: HomePageComponent
+  },
+  { 
+    path: 'products', component: ProductListPageComponent
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HomePageComponent, ProductListPageComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
